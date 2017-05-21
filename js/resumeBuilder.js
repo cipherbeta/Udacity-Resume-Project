755788dd33e11a723e8a13e2c0962aa006363d5f
IsaacDoud <ikedoud45@gmail.com>
2017-05-20 22:13:37 -0500

    Fixed JShint error with insertData();

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -7,7 +7,7 @@ var emptyCheck = function(jsonElement) {
 };
 
 // Streamlines insert process to streamline code
-var insertData = function(formattedData, originalData, referencePoint, insertPoint, insertStyle) {
+var insertData = function(originalData, referencePoint, insertPoint, insertStyle) {
   var formattedData = originalData.replace("%data%", referencePoint);
   $(insertPoint)[insertStyle](formattedData);
 };
@@ -17,25 +17,25 @@ var insertData = function(formattedData, originalData, referencePoint, insertPoi
 // Add .display funtion to bio variable so we can call it later.
 bio.display = function() {
   if (emptyCheck(bio.name)) {
-    insertData("formattedHeaderName", HTMLheaderName, bio.name, "#header", "prepend");
+    insertData(HTMLheaderName, bio.name, "#header", "prepend");
   }
 
   if (emptyCheck(bio.role)) {
-    insertData("formattedHeaderRole", HTMLheaderRole, bio.role, "#name", "after");
+    insertData(HTMLheaderRole, bio.role, "#name", "after");
   }
 
   if (emptyCheck(bio.biopic)) {
-    insertData("formattedBioPic", HTMLbioPic, bio.biopic, "#header", "append");
+    insertData(HTMLbioPic, bio.biopic, "#header", "append");
   }
 
   if (emptyCheck(bio.welcome)) {
-    insertData("formattedWelcomeMsg", HTMLwelcomeMsg, bio.welcome, "#header", "append");
+    insertData(HTMLwelcomeMsg, bio.welcome, "#header", "append");
   }
   // Append list of skills to header
   if (emptyCheck(bio.skills)) {
     $("#header").append(HTMLskillsStart);
     for (skill = 0; skill < bio.skills.length; skill++) {
-      insertData("formattedSkill", HTMLskills, bio.skills[skill], "#skills", "append");
+      insertData(HTMLskills, bio.skills[skill], "#skills", "append");
     }
   }
 };
@@ -44,27 +44,27 @@ bio.display = function() {
 bio.contacts.display = function(appendLocation) {
 
   if (emptyCheck(bio.contacts.mobile)) {
-    insertData("formattedHTMLmobile", HTMLmobile, bio.contacts.mobile, appendLocation, "append");
+    insertData(HTMLmobile, bio.contacts.mobile, appendLocation, "append");
   }
 
   if (emptyCheck(bio.contacts.email)) {
-    insertData("formattedHTMLemail", HTMLemail, bio.contacts.email, appendLocation, "append");
+    insertData(HTMLemail, bio.contacts.email, appendLocation, "append");
   }
 
   if (emptyCheck(bio.contacts.twitter)) {
-    insertData("formattedHTMLtwitter", HTMLtwitter, bio.contacts.twitter, appendLocation, "append");
+    insertData(HTMLtwitter, bio.contacts.twitter, appendLocation, "append");
   }
 
   if (emptyCheck(bio.contacts.github)); {
-    insertData("formattedHTMLgithub", HTMLgithub, bio.contacts.github, appendLocation, "append");
+    insertData(HTMLgithub, bio.contacts.github, appendLocation, "append");
   }
 
   if (emptyCheck(bio.contacts.blog)) {
-    insertData("formattedHTMLblog", HTMLblog, bio.contacts.blog, appendLocation, "append");
+    insertData(HTMLblog, bio.contacts.blog, appendLocation, "append");
   }
 
   if (emptyCheck(bio.contacts.mobile)) {
-    insertData("formattedHTMLlocation", HTMLlocation, bio.contacts.location, appendLocation, "append");
+    insertData(HTMLlocation, bio.contacts.location, appendLocation, "append");
   }
 };
 
@@ -74,23 +74,23 @@ work.display = function() {
     $("#workExperience").append(HTMLworkStart);
 
     if (emptyCheck(work.jobs[job].employer)) {
-      insertData("formattedHTMLworkEmployer", HTMLworkEmployer, work.jobs[job].employer, ".work-entry:last", "append");
+      insertData(HTMLworkEmployer, work.jobs[job].employer, ".work-entry:last", "append");
     }
 
     if (emptyCheck(work.jobs[job].dates)) {
-      insertData("formattedHTMLworkDates", HTMLworkDates, work.jobs[job].dates, ".work-entry:last", "append");
+      insertData(HTMLworkDates, work.jobs[job].dates, ".work-entry:last", "append");
     }
 
     if (emptyCheck(work.jobs[job].location)) {
-      insertData("formattedHTMLworkLocation", HTMLworkLocation, work.jobs[job].location, ".work-entry:last", "append");
+      insertData(HTMLworkLocation, work.jobs[job].location, ".work-entry:last", "append");
     }
 
     if (emptyCheck(work.jobs[job].title)) {
-      insertData("formattedHTMLworkTitle", HTMLworkTitle, work.jobs[job].title, ".work-entry a:last", "append");
+      insertData(HTMLworkTitle, work.jobs[job].title, ".work-entry a:last", "append");
     }
 
     if (emptyCheck(work.jobs[job].description)) {
-      insertData("formattedHTMLworkDescription", HTMLworkDescription, work.jobs[job].description, ".work-entry:last", "append");
+      insertData(HTMLworkDescription, work.jobs[job].description, ".work-entry:last", "append");
     }
   }
 };
@@ -101,20 +101,20 @@ projects.display = function() {
     $("#projects").append(HTMLprojectStart);
 
     if (emptyCheck(projects.projects[project].title)) {
-      insertData("formattedHTMLprojectTitle", HTMLprojectTitle, projects.projects[project].title, ".project-entry:last", "append");
+      insertData(HTMLprojectTitle, projects.projects[project].title, ".project-entry:last", "append");
     }
 
     if (emptyCheck(projects.projects[project].dates)) {
-      insertData("formattedHTMLprojectDates", HTMLprojectDates, projects.projects[project].dates, ".project-entry:last", "append");
+      insertData(HTMLprojectDates, projects.projects[project].dates, ".project-entry:last", "append");
     }
 
     if (emptyCheck(projects.projects[project].title)) {
-      insertData("formattedHTMLprojectDescription", HTMLprojectDescription, projects.projects[project].description, ".project-entry:last", "append");
+      insertData(HTMLprojectDescription, projects.projects[project].description, ".project-entry:last", "append");
     }
 
     if (emptyCheck(projects.projects[project].images)) {
       for (image = 0; image < projects.projects[project].images.length; image++) {
-        insertData("formattedHTMLprojectImage", HTMLprojectImage, projects.projects[project].images[image], ".project-entry:last", "append");
+        insertData(HTMLprojectImage, projects.projects[project].images[image], ".project-entry:last", "append");
       }
     }
   }
@@ -127,7 +127,7 @@ education.display = function() {
     $("#education").append(HTMLschoolStart);
 
     if (emptyCheck(education.schools[school].name)) {
-      insertData("formattedHTMLschoolName", HTMLschoolName, education.schools[school].name, ".education-entry:last", "append");
+      insertData(HTMLschoolName, education.schools[school].name, ".education-entry:last", "append");
     }
 
     // Change href value for name to match url
@@ -136,19 +136,19 @@ education.display = function() {
     }
 
     if (emptyCheck(education.schools[school].degree)) {
-      insertData("formattedHTMLschoolDegree", HTMLschoolDegree, education.schools[school].degree, ".education-entry:last a:first-child", "append");
+      insertData(HTMLschoolDegree, education.schools[school].degree, ".education-entry:last a:first-child", "append");
     }
 
     if (emptyCheck(education.schools[school].dates)) {
-      insertData("formattedHTMLschoolDates", HTMLschoolDates, education.schools[school].dates, ".education-entry:last a:first-child", "after");
+      insertData(HTMLschoolDates, education.schools[school].dates, ".education-entry:last a:first-child", "after");
     }
 
     if (emptyCheck(education.schools[school].location)) {
-      insertData("formattedHTMLschoolLocation", HTMLschoolLocation, education.schools[school].location, ".education-entry:last .date-text", "after");
+      insertData(HTMLschoolLocation, education.schools[school].location, ".education-entry:last .date-text", "after");
     }
 
     if (emptyCheck(education.schools[school].majors)) {
-      insertData("formattedHTMLschoolMajor", HTMLschoolMajor, education.schools[school].majors, ".education-entry:last .date-text", "after");
+      insertData(HTMLschoolMajor, education.schools[school].majors, ".education-entry:last .date-text", "after");
     }
   }
 
@@ -158,16 +158,16 @@ education.display = function() {
   for (course = 0; course < education.onlineCourses.length; course++) {
     $("#education").append(HTMLschoolStart);
     if (emptyCheck(education.onlineCourses[course].title)) {
-      insertData("formattedHTMLonlineTitle", HTMLonlineTitle, education.onlineCourses[course].title, ".education-entry:last", "append");
+      insertData(HTMLonlineTitle, education.onlineCourses[course].title, ".education-entry:last", "append");
     }
     if (emptyCheck(education.onlineCourses[course].school)) {
-      insertData("formattedHTMLonlineSchool", HTMLonlineSchool, education.onlineCourses[course].school, ".education-entry a:last", "append");
+      insertData(HTMLonlineSchool, education.onlineCourses[course].school, ".education-entry a:last", "append");
     }
     if (emptyCheck(education.onlineCourses[course].dates)) {
-      insertData("formattedHTMLonlineDates", HTMLonlineDates, education.onlineCourses[course].dates, ".education-entry:last", "append");
+      insertData(HTMLonlineDates, education.onlineCourses[course].dates, ".education-entry:last", "append");
     }
     if (emptyCheck(education.onlineCourses[course].url)) {
-      insertData("formattedHTMLonlineURL", HTMLonlineURL, education.onlineCourses[course].url, ".education-entry:last", "append");
+      insertData(HTMLonlineURL, education.onlineCourses[course].url, ".education-entry:last", "append");
     }
   }
 };
